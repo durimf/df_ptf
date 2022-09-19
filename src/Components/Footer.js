2dffc9e625f32b8f4484ec601f1904fcd2cc90aa
qwerty <durimfetahaj@gmail.com>
2022-09-19 14:00:13 +0200

    margin left added to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,11 @@ import { Box } from '@mui/material'
 import React from 'react'
 export const Footer = () => {
   return (
-    <Box bgcolor={'palette.background.default'} color={'text.primary'}>
+    <Box
+      bgcolor={'palette.background.default'}
+      color={'text.primary'}
+      marginLeft={1}
+    >
       <span>© all rights reserved. powered by DF. 2022</span>
     </Box>
   )
