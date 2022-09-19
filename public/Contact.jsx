164311229beef76b4edd1e616cdb5cb1d1715b53
duro.codes <59978280+durimf@users.noreply.github.com>
2022-09-19 14:45:56 +0200

    Add files via upload

---
+++ public/Contact.jsx
@@ -0,0 +1,14 @@
+import { React } from 'react'
+import ContactForm from './ContactForm'
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div className="contact-main">
+        <ContactForm />
+      </div>
+    </div>
+  )
+}
+
+export default Contact
